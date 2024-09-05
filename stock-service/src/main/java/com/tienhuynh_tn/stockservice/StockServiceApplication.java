608d6011e96477431e0bc5da10c867138f5fc30e
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-09-05 22:41:43 +0700

    Refactor email, order, stock services

---
+++ stock-service/src/main/java/com/tienhuynh_tn/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tienhuynh_tn.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
---
+++ stock-service/src/test/java/com/tienhuynh_tn/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienhuynh_tn.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
