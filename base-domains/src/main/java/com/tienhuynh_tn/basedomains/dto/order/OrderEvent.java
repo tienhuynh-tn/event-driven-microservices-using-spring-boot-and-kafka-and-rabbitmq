358e0942c0fec50a16669707429f23e0c1053f29
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-09-07 22:05:27 +0700

    Add order dto

---
+++ base-domains/src/main/java/com/tienhuynh_tn/basedomains/dto/order/OrderEvent.java
@@ -0,0 +1,14 @@
+package com.tienhuynh_tn.basedomains.dto.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent {
+    private String message;
+    private String status;
+    private Order order;
+}
