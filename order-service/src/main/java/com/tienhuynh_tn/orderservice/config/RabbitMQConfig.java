831d6840ff8ecd5efd0e2778cd1a8259263be64c
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-09-08 23:10:15 +0700

    Configure RabbitMQ

---
+++ order-service/src/main/java/com/tienhuynh_tn/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,58 @@
+package com.tienhuynh_tn.orderservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.queue.name.email}")
+    private String orderEmailQueue;
+
+    @Value("${rabbitmq.binding.routing.key.email}")
+    private String orderEmailRoutingKey;
+
+    // Spring bean for exchange
+    @Bean
+    public Exchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Spring bean for queue - order queue
+    @Bean
+    public Queue orderEmailQueue() {
+        return new Queue(orderEmailQueue);
+    }
+
+    // Spring bean for binding between exchange and queue using routing key
+    @Bean
+    public Binding orderEmailBinding() {
+        return BindingBuilder
+                .bind(orderEmailQueue())
+                .to(exchange())
+                .with(orderEmailRoutingKey).noargs();
+    }
+
+    // Message converter
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Configure RabbitTemplate
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
