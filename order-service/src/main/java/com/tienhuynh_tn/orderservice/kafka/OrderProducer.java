75a5c6c7080120269d8e92e60c7366910e023c1b
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2024-09-08 15:47:26 +0700

    Config OrderProducer

---
+++ order-service/src/main/java/com/tienhuynh_tn/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,37 @@
+package com.tienhuynh_tn.orderservice.kafka;
+
+import com.tienhuynh_tn.basedomains.dto.order.OrderEvent;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    private NewTopic topic;
+
+    private KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    public OrderProducer(NewTopic topic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.topic = topic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Order event => %s", orderEvent.toString()));
+
+        // Create message
+        Message<OrderEvent> message = MessageBuilder
+                .withPayload(orderEvent)
+                .setHeader(KafkaHeaders.TOPIC, topic.name())
+                .build();
+        kafkaTemplate.send(message);
+    }
+}
